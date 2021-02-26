d94f347459851e1660b2215f0c718eff7fc600b3
JohnAzedo <jplimao12@gmail.com>
2021-02-26 14:34:14 -0300

    Create model structure

---
+++ main.py
@@ -0,0 +1,37 @@
+# %%
+from typing import List
+from datetime import datetime as Datetime
+
+
+# %%
+class Client:
+    _private_key: str
+    public_key: str
+    address: str
+
+
+# %%
+# Encrypted fields : [text, date]
+
+class Message:
+    text: str
+    date: Datetime
+    sender: Client
+
+
+# %%
+class Chat:
+    _messages: List[Message]
+    _participants: List[Client]
+
+    
+
+# %%
+# TODO: Change it
+class Server:
+    _chats: List[Chat]
+    _clients: List[Client]
+
+
+# %%
+# Exec
